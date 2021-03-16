245c951d23362cfee405e087f061b6ede3b08742
idembele70 <idembele70@gmail.com>
2021-03-16 14:09:28 +0100

    first-comit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace W3course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 10;
+
+            System.Console.WriteLine(x + y);
+        }
+    }
+}
